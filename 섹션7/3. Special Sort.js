b6800cc6663d2c94e55b80f4a5e5e121243bf3e1
brandon <zxczoxc125@naver.com>
2021-04-10 13:40:32 +0900

    ✨ 3. Special Sort

---
+++ 섹션7/3. Special Sort.js
@@ -0,0 +1,15 @@
+function solution(arr = []) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(solution([1, 2, 3, -3, -2, 5, 6, -6]));
