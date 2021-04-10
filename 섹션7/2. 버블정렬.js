b310e775b77b062ac1bda39a3ea2091d81795b4b
brandon <zxczoxc125@naver.com>
2021-04-10 13:20:58 +0900

    ✨ 2. 버블정렬

---
+++ 섹션7/2. 버블정렬.js
@@ -0,0 +1,15 @@
+function solution(arr = []) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
