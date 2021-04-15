ab3d6cea16fcabeba3e92ea38618c19690a819df
brandon <zxczoxc125@naver.com>
2021-04-15 20:43:14 +0900

    ⚡️ 3. 연속부분수열1

--- 섹션5/3. 연속부분수열1.js
+++ 섹션5/3. 연속부분수열1.js
@@ -1,22 +1,47 @@
-function solution(num, arr = []) {
+// [풀이 보고 나서]
+function solution(n, arr = []) {
   let count = 0;
+  let lt = 0;
+  let sum = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    let sum = arr[i];
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum === n) {
+      count++;
+    }
 
-    for (let j = i + 1; j < arr.length; j++) {
-      sum += arr[j];
-
-      if (sum === num) {
-        count++;
-        break;
-      }
+    while (sum >= n) {
+      lt++;
+      sum -= arr[lt];
+      if (sum === n) count++;
     }
   }
 
   return count;
 }
 
+// 6, [1, 2, 1, 3, 1, 1, 1, 2]
+
+// [기존 풀이]
+// function solution(num, arr = []) {
+//   let count = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let sum = arr[i];
+
+//     for (let j = i + 1; j < arr.length; j++) {
+//       sum += arr[j];
+
+//       if (sum === num) {
+//         count++;
+//         break;
+//       }
+//     }
+//   }
+
+//   return count;
+// }
+
 console.log(solution(6, [1, 2, 1, 3, 1, 1, 1, 2]));
 
-// 풀이는 안쪽 반복은 while로 해서 break X
+// rt를 루프의 기준으로 잡는 걸 생각할수 있어야 한다.
