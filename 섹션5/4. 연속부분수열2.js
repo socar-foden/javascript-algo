49a5eb5791b598b04a1973d1c6be36f3a42f41fe
brandon <zxczoxc125@naver.com>
2021-04-16 09:02:33 +0900

    ⚡️ 4. 연속부분수열2

--- 섹션5/4. 연속부분수열2.js
+++ 섹션5/4. 연속부분수열2.js
@@ -1,24 +1,61 @@
-function solution(num, arr = []) {
+// [풀이 보고 난 후]
+function solution(n, arr = []) {
   let count = 0;
+  let sum = 0;
+  let lt = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    let sum = arr[i];
-    let sub_count = 0;
-    let j = i;
-
-    while (sum <= num) {
-      j++;
-      sum += arr[j];
-      sub_count++;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum <= n) {
+      count++;
     }
 
-    count += sub_count;
+    while (sum >= n) {
+      lt++;
+      sum -= arr[lt];
+      count += lt;
+    }
   }
 
   return count;
 }
 
+// 1 -> o
+// 1 3 -> o
+// 1 3 1 -> o
+// 1 3 1 2 -> x
+//   3 1 2 -> x
+//   3 1 -> o
+//   3 -> o
+//     1 -> o
+//     1 2 -> o
+//     1 2 3 -> x
+//       2 3 -> o
+//         3 -> o
+
+// [기존 풀이]
+// function solution(num, arr = []) {
+//   let count = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let sum = arr[i];
+//     let sub_count = 0;
+//     let j = i;
+
+//     while (sum <= num) {
+//       j++;
+//       sum += arr[j];
+//       sub_count++;
+//     }
+
+//     count += sub_count;
+//   }
+
+//   return count;
+// }
+
 console.log(solution(5, [1, 3, 1, 2, 3]));
 
 // 풀의 강의 봐야함
-// 내가 푼 방식이 투 포인터 알고리즘의 개념과 같긴 한거 같다.
+// 내가 푼 방식이 투 포인터 알고리즘의 개념과 같긴 한거 같다. --> X
+// 투 포인터 알고리즘의 핵심은 복잡도가 n이라는데 의의가 있다.
